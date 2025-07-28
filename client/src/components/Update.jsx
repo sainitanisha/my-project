d320f139fd8ebc768989f4d0068abf0c5f3eb4a9
sainitanisha <sainitanisha223@gmail.com>
2025-07-28 14:59:23 +0530

    Update Update.jsx

--- client/src/components/Update.jsx
+++ client/src/components/Update.jsx
@@ -13,7 +13,7 @@ export function Update(){
         e.preventDefault();
         try
         {
-const response = await axios.put(`http://localhost:3001/api/employees/${id}`, { empNo: Number(empNo),empName, empSal: Number(empSal) });
+const response = await axios.put(`https://tanisha-emp.onrender.com/api/employees/${id}`, { empNo: Number(empNo),empName, empSal: Number(empSal) });
             alert(response.data.message);
         }
         catch(err)
